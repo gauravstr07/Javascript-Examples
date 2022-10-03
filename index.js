4594cfc1ef4b75027929217166f63eaa6b081269
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-03 14:30:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Calling index.js');
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function generateString(length){
+    let result = '';
+    const charactersLength = characters.length;
+    for(let i = 0; i < length; i++){
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+console.log(generateString(5));
